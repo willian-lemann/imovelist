1050eaa97150554060301949df7089f374b9cb22
willian-lemann <willianleman@hotmail.com>
2025-03-08 22:41:26 -0300

    feat: solve build

--- database/factories/test_factory.ts
+++
@@ -1,8 +0,0 @@
-import factory from '@adonisjs/lucid/factories'
-import Test from '#models/test'
-
-export const TestFactory = factory
-  .define(Test, async ({ faker }) => {
-    return {}
-  })
-  .build()
